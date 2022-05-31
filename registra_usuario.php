986dd4bb1c5cf24aea1042f36b287bea4f074280
Nicollas <nicksz55@hotmail.com>
2022-05-31 15:03:38 -0300

    Mudanças

--- registra_usuario.php
+++ registra_usuario.php
@@ -1,23 +1,64 @@
 <?php
 
-  require_once('db.class.php');
+require_once('db.class.php');
 
-  $usuario = $_POST['usuario'];
-  $email = $_POST['email'];
-  $senha = md5($_POST['senha']);
+$usuario = $_POST['usuario'];
+$email = $_POST['email'];
+$senha = md5($_POST['senha']);
 
-  $objDb = new db();
-  $link = $objDb -> conecta_mysql();
+$objDb = new db();
+$link = $objDb->conecta_mysql();
 
-  $sql = "INSERT INTO usuarios(usuario, email, senha) VALUES ('$usuario', '$email', '$senha')";
+$usuario_existe = false;
+$email_existe = false;
 
-  //executar a query
-  if(mysqli_query($link, $sql)){
+//verificar se o usuário já existe
 
-    echo 'Usuário registrado com sucesso!';
+$sql = "SELECT * FROM usuarios WHERE usuario = '$usuario'";
+if ($resultado_id = mysqli_query($link, $sql)) {
 
-  } else {
+  $dados_usuario = mysqli_fetch_array($resultado_id);
 
-    echo 'Erro ao registrar o usuário!';
+  if (isset($dados_usuario['usuario'])) {
 
+    $usuario_existe = true;
   }
+} else {
+
+  echo 'Erro ao tentar loalizar o registro do usuário.';
+}
+
+//verificar se o email já existe
+
+$sql = "SELECT * FROM usuarios WHERE email = '$email'";
+if ($resultado_id = mysqli_query($link, $sql)) {
+
+  $dados_usuario = mysqli_fetch_array($resultado_id);
+
+  if (isset($dados_usuario['email'])) {
+
+    $email_existe = true;
+  }
+} else {
+
+  echo 'Erro ao tentar loalizar o registro de email.';
+}
+
+if($usuario_existe || $email_existe){
+
+  header('Location: inscrevase.php');
+
+}
+
+die();
+
+$sql = "INSERT INTO usuarios(usuario, email, senha) VALUES ('$usuario', '$email', '$senha')";
+
+//executar a query
+if (mysqli_query($link, $sql)) {
+
+  echo 'Usuário registrado com sucesso!';
+} else {
+
+  echo 'Erro ao registrar o usuário!';
+}
