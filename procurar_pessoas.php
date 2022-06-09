19232724fa81f9242bdf2f34442b7e2be8a5c3df
Nicollas <nicksz55@hotmail.com>
2022-06-09 17:51:35 -0300

    Novas funções e mudanças

--- procurar_pessoas.php
+++ procurar_pessoas.php
@@ -39,6 +39,55 @@ if (!isset($_SESSION['usuario'])) {
 
               $('#pessoas').html(data);
 
+              $('.btn_seguir').click(function() {
+
+                var id_usuario = $(this).data('id_usuario');
+
+                $('#btn_seguir_' +id_usuario).hide();
+                $('#btn_deixar_seguir_' +id_usuario).show();
+
+                $.ajax({
+
+                  url: 'seguir.php',
+                  method: 'post',
+                  data: {
+                    seguir_id_usuario: id_usuario
+                  },
+                  success: function(data) {
+
+
+
+                  }
+
+                });
+
+              });
+
+              $('.btn_deixar_seguir').click(function() {
+
+                var id_usuario = $(this).data('id_usuario');
+
+                $('#btn_seguir_' +id_usuario).show();
+                $('#btn_deixar_seguir_' +id_usuario).hide();
+
+                $.ajax({
+
+                  url: 'deixar_seguir.php',
+                  method: 'post',
+                  data: {
+                    deixar_seguir_id_usuario: id_usuario
+                  },
+                  success: function(data) {
+
+
+
+                  }
+
+                });
+
+
+              });
+
             }
 
           });
