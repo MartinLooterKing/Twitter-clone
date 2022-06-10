5f80a3ca6d8999940777cb4e7a3001c57fa874fa
Nicollas <nicksz55@hotmail.com>
2022-06-10 15:24:25 -0300

    Projeto prático do curso concluído

--- procurar_pessoas.php
+++ procurar_pessoas.php
@@ -7,6 +7,43 @@ if (!isset($_SESSION['usuario'])) {
   header('Location: index.php?erro=1');
 }
 
+require_once('db.class.php');
+
+$objDb = new db();
+$link = $objDb->conecta_mysql();
+
+$id_usuario = $_SESSION['id_usuario'];
+
+//--qtde de tweets
+$sql = "SELECT COUNT(*) AS qtde_tweets FROM tweet WHERE id_usuario = $id_usuario ";
+
+$resultado_id = mysqli_query($link, $sql);
+
+$qtde_tweets = 0;
+
+if ($resultado_id) {
+  $registro = mysqli_fetch_array($resultado_id, MYSQLI_ASSOC);
+
+  $qtde_tweets = $registro['qtde_tweets'];
+} else {
+  echo 'Erro ao executar a query';
+}
+
+//--qtde de seguidores
+$sql = "SELECT COUNT(*) AS qtde_seguidores FROM usuarios_seguidores WHERE seguindo_id_usuario = $id_usuario ";
+
+$resultado_id = mysqli_query($link, $sql);
+
+$qtde_seguidores = 0;
+
+if ($resultado_id) {
+  $registro = mysqli_fetch_array($resultado_id, MYSQLI_ASSOC);
+
+  $qtde_seguidores = $registro['qtde_seguidores'];
+} else {
+  echo 'Erro ao executar a query';
+}
+
 ?>
 
 <!DOCTYPE HTML>
@@ -43,8 +80,8 @@ if (!isset($_SESSION['usuario'])) {
 
                 var id_usuario = $(this).data('id_usuario');
 
-                $('#btn_seguir_' +id_usuario).hide();
-                $('#btn_deixar_seguir_' +id_usuario).show();
+                $('#btn_seguir_' + id_usuario).hide();
+                $('#btn_deixar_seguir_' + id_usuario).show();
 
                 $.ajax({
 
@@ -67,8 +104,8 @@ if (!isset($_SESSION['usuario'])) {
 
                 var id_usuario = $(this).data('id_usuario');
 
-                $('#btn_seguir_' +id_usuario).show();
-                $('#btn_deixar_seguir_' +id_usuario).hide();
+                $('#btn_seguir_' + id_usuario).show();
+                $('#btn_deixar_seguir_' + id_usuario).hide();
 
                 $.ajax({
 
@@ -137,10 +174,10 @@ if (!isset($_SESSION['usuario'])) {
 
           <hr>
           <div class="col-md-6">
-            TWEETS <br> 1
+            TWEETS <br> <?= $qtde_tweets ?>
           </div>
           <div class="col-md-6">
-            SEGUIDORES <br> 1
+            SEGUIDORES <br> <?= $qtde_seguidores ?>
           </div>
         </div>
 
